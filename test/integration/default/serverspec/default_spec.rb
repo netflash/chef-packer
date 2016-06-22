c78a8118c8ccd9710058d3342940de5c2306443d
Alex Romanov <alex@strataconsulting.com>
2016-06-22 09:26:34 -0700

    Rubocop warnings

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,7 +4,6 @@ describe 'devopsbox::default' do
   
   # expect packer to be installed
   describe command('/usr/local/bin/packer version') do
-    its(:stdout) { should match /^Packer v0\.10\.1.*$/ }
+    its(:stdout) { should match / ^Packer v0\.10\.1.*$/ }
   end
-
 end
