abd94f2aa5e956fd1d81bed52b56cbf2cf7a3d84
Alex Romanov <alex@strataconsulting.com>
2016-06-22 09:36:52 -0700

    Revert extra space

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,6 +4,6 @@ describe 'devopsbox::default' do
   
   # expect packer to be installed
   describe command('/usr/local/bin/packer version') do
-    its(:stdout) { should match / ^Packer v0\.10\.1.*$/ }
+    its(:stdout) { should match /^Packer v0\.10\.1.*$/ }
   end
 end
